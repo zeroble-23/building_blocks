f836109d075cafccc1cdab2abe5127a0922bf441
Zeroble23 <11418177+zeroble-23@users.noreply.github.com>
2019-02-07 21:38:52 +0900

    Add game files

---
+++ Building Blocks/Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Block : MonoBehaviour {
+    public float health = 10;
+
+    void OnCollisionEnter(Collision collision) {
+        health -= collision.relativeVelocity.magnitude;
+
+        if(health <= 0) {
+            Destroy(gameObject);
+        }
+
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        if (boxes.Length <= 1) {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+
+
+}
---
+++ Building Blocks/Assets/Scripts/Cannon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject cannonballPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        if (Input.GetButtonDown("Fire1")) {
+            GameObject cannonball = Instantiate(cannonballPrefab, transform.position, transform.rotation) as GameObject;
+            cannonball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, 2000));
+
+        }
+    }
+
+    void LateUpdate() {
+        float x = Input.GetAxis("Mouse X") * 2;
+        float y = -Input.GetAxis("Mouse Y");
+
+        // vertical tilting
+        float yClamped = transform.eulerAngles.x + y;
+        transform.rotation = Quaternion.Euler(yClamped, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        // horizontal orbiting
+        transform.RotateAround(new Vector3(0, 3, 0), Vector3.up, x);
+    }
+}
